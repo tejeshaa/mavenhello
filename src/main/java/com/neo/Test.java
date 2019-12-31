63e95d642de2cdf029237702e81f4492b0003848
ravik-123 <ravi.k@neosofttech.com>
2019-12-31 17:50:40 +0530

    test class updated

--- src/main/java/com/neo/Test.java
+++ src/main/java/com/neo/Test.java
@@ -1,8 +1,12 @@
+/************
+ * ********
+ */
 package com.neo;
 
 public class Test {
 int a = 100;
 
+//todo display the data 
  public void display(){
 	 System.out.println("hello this is test ");
  }
